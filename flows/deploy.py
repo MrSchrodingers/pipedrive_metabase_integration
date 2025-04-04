ec7f2af749666bf3ed9290a74ded4204605518b7
Matheus Munhoz <mrschrodingers@gmail.com>
2025-04-04 18:10:35 -0300

    chore: Atualiza job e deployment prefect para trabalhar diretamente em ambiente Kubernetes.

--- flows/deploy.py
+++ flows/deploy.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
-from prefect import serve
 from prefect.schedules import Interval
 from flows.pipedrive_metabase_etl import main_etl_flow
+from prefect_kubernetes.jobs import KubernetesJob
 
 if __name__ == "__main__":
     deployment = main_etl_flow.to_deployment(
@@ -10,8 +10,25 @@ if __name__ == "__main__":
         tags=["pipedrive", "etl", "database"],
         parameters={"run_batch_size": 1500},
         schedule=Interval(
-            timedelta(hours=12),
+            timedelta(minutes=30),
+        ),
+        infrastructure=KubernetesJob(
+            image="pipedrive_metabase_integration-etl:latest",
+            image_pull_policy="Never",
+            env={"PREFECT_API_URL": "http://prefect-orion:4200/api"}, 
+            env_from=[
+                {
+                    "secretRef": {
+                        "name": "app-secrets" 
+                    }
+                },
+                {
+                    "secretRef": {
+                        "name": "db-secrets"
+                    }
+                }
+            ]
         )
     )
 
-    serve(deployment)
+    # serve(deployment)
