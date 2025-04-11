02ea10caf12c25a76075059efa9690a3abc07afe
Matheus Munhoz <mrschrodingers@gmail.com>
2025-04-11 18:00:39 -0300

    debug(etl): Add debug logs pre and after custom fields to map addresses #5.

--- application/services/etl_service.py
+++ application/services/etl_service.py
@@ -399,6 +399,7 @@ class ETLService:
                             load_start = time.monotonic()
                             try:
                                 with db_operation_duration_hist.labels(operation='upsert').time():
+                                    batch_log.debug("Sending batch to repository upsert.", first_ids=[rec.get("id") for rec in validated_batch[:5]])
                                     self.repository.save_data_upsert(validated_batch)
                                 current_loaded_count = len(validated_batch)
                                 total_loaded += current_loaded_count
@@ -445,6 +446,7 @@ class ETLService:
                         load_start = time.monotonic()
                         try:
                             with db_operation_duration_hist.labels(operation='upsert').time():
+                                batch_log.debug("Sending batch to repository upsert.", first_ids=[rec.get("id") for rec in validated_batch[:5]])
                                 self.repository.save_data_upsert(validated_batch)
                             current_loaded_count = len(validated_batch)
                             total_loaded += current_loaded_count
