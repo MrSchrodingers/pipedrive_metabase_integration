f48572e7af1be8ba417adf21b048d468cb4e5c6d
Matheus Munhoz <mrschrodingers@gmail.com>
2025-04-25 15:52:16 -0300

    fix: ensure worker entrypoint permissions in Docker runtime image.

--- infrastructure/prefect/worker/worker_entrypoint.sh
+++ infrastructure/prefect/worker/worker_entrypoint.sh
@@ -5,12 +5,11 @@ POOL=docker-pool
 NET=prefect_internal_network
 
 # 1. Cria/atualiza o pool com configurações Docker
-prefect work-pool create --name "$POOL" --type docker --overwrite \
-  --config '{
-    "image": "mrschrodingers/pmi-runtime:latest",
-    "network": "'"$NET"'",
-    "host": "unix:///var/run/docker.sock"
-  }'
+prefect work-pool create \
+  --type docker \
+  --default-docker-image "mrschrodingers/pmi-runtime:latest" \
+  --default-docker-network "prefect_internal_network" \
+  docker-pool
 
 # 2. Inicia o worker
-prefect worker start --work-pool "$POOL"
+prefect worker start --pool docker-pool --network prefect_internal_network
